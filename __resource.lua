0d18c96b513c8a65998eac45da2ce495078983bc
trulshjalmar <86436012+trulshjalmar@users.noreply.github.com>
2023-02-21 14:29:08 +0100

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -2,6 +2,6 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 author 'Julls Hjalmar <truls.hjalmar@hotmail.com>'
 description 'Norsk kart Fivem'
-version '1.0.3'
+version '1.0.7'
 
 client_script "client.lua"
